45ba54ec813223e18473bced6775d79fb4b67c22
KingElrond <66854137+KingElrond@users.noreply.github.com>
2021-03-08 18:20:51 -0800

    idk anymore

--- MagicSquaresProgram(Troy)/src/magicsquaresprogram/troy/Main.java
+++ MagicSquaresProgram(Troy)/src/magicsquaresprogram/troy/Main.java
@@ -11,11 +11,74 @@ package magicsquaresprogram.troy;
  */
 public class Main {
 
+	
+	public static int[][] squareArr = {
+			{2,7,6},
+			{9,5,1},
+			{4,3,8},
+	};
+	
+	
+	
+	static int[] sumOfEachRow(int[][] square)
+	{
+		int[] rowsums = new int[square.length];
+		
+		for(int row=0; row<square.length; row++){
+            for(int col=0; col<square[row].length;col++){
+                int columnValue = square[row][col];
+                rowsums[row] = rowsums[row] + columnValue;
+            }
+		}
+		return rowsums;
+	}
+	
+	
+	static int[] getColumn(int[][] square)
+	{
+		return null;
+		
+	}
+	
+	
+	static int[] sumOfEachColumn(int[][] square, int c)
+	{
+		return null;
+		
+	}
+	
+	
+	static int sumOfDescendingDiagonal(int[][] square)
+	{
+		return 0;
+		
+	}
+	
+	
+	static int sumOfAscendingDiagonal(int[][] square)
+	{
+		return 0;
+		
+	}
+	
+	
+	static boolean isMagicSquare(int[] rowSum, int[] colSum, int ascendingDiagonal, int descendingDiagonal)
+	{
+		return false;
+		
+	}
+	
+	
+	
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    	sumOfEachRow(squareArr);
+    	
+    	
+    	
     }
     
 }
