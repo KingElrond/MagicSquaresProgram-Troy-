3ebd6edee4fa5028906e8a531fa7cc64d27a5e3c
KingElrond <66854137+KingElrond@users.noreply.github.com>
2021-03-10 19:24:35 -0800

    Finished(again) it does everything its supposed to.

--- MagicSquaresProgram(Troy)/src/magicsquaresprogram/troy/Main.java
+++ MagicSquaresProgram(Troy)/src/magicsquaresprogram/troy/Main.java
@@ -13,9 +13,10 @@ public class Main {
 
 	
 	public static int[][] squareArr = {
-			{1,1,6},
-			{9,5,1},
-			{4,3,8},
+			{1,15,14,4},
+			{10,11,8,5},
+			{7,6,9,12},
+			{16,2,3,13},
 	};
 	
 	
@@ -34,9 +35,18 @@ public class Main {
 	}
 	
 	
-	static int[] getColumn(int[][] square)
+	static int[] getColumn(int[][] square, int c)
 	{
-		return null;
+		//What is this for?
+		int[] column = new int[square.length];
+		
+		for(int row=0; row<square.length; row++)
+		{
+			int colVal=square[row][c];
+			column[row]=colVal;
+		}
+		
+		return column;
 		
 	}
 	
@@ -92,7 +102,70 @@ public class Main {
 	
 	static boolean isMagicSquare(int[] rowSum, int[] colSum, int ascendingDiagonal, int descendingDiagonal)
 	{
-		return false;
+		boolean rowsEqual = true, colsEqual = true, rowcolEqual=true, diagrowEqual=true, diagEqual=true, allEqual = false;
+		for(int g: rowSum)
+		{
+			for(int g2: rowSum)
+			{
+				if(g!=g2)
+				{
+					rowsEqual=false;
+				}
+			}
+		}
+		
+		for(int g: colSum)
+		{
+			for(int g2: colSum)
+			{
+				if(g!=g2)
+				{
+					colsEqual=false;
+				}
+			}
+		}
+		
+		for(int g: colSum)
+		{
+			for(int g2: rowSum)
+			{
+				if(g!=g2)
+				{
+					rowcolEqual=false;
+				}
+			}
+		}
+		
+		for(int g: rowSum)
+		{
+			for(int g2: rowSum)
+			{
+				if(g!=g2)
+				{
+					rowsEqual=false;
+				}
+			}
+		}
+		if(descendingDiagonal!=ascendingDiagonal)
+		{
+			diagEqual=false;
+		}
+		
+		for(int g: rowSum)
+		{
+			int g2=descendingDiagonal;
+				if(g!=g2)
+				{
+					diagrowEqual=false;
+				}
+		}
+		
+		if(rowsEqual&&colsEqual&&rowcolEqual&&diagEqual&&diagrowEqual)
+		{
+			allEqual=true;
+		}
+		
+		return allEqual;
 		
 	}
 	
@@ -103,22 +176,27 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    	System.out.println("Square is: " + squareArr.length + "x" + squareArr.length);
+    	System.out.println("Rowsums: ");
     	int[] rowsum=sumOfEachRow(squareArr);
         for(int g : rowsum)
         {
             System.out.println(g);
         }
+        System.out.println("Column sums: ");
     	int[] colsum=sumOfEachColumn(squareArr,0);
         for(int g : colsum)
         {
             System.out.println(g);
         }
-    	
         int aDsum = sumOfAscendingDiagonal(squareArr);
         int dDsum = sumOfDescendingDiagonal(squareArr);
-        System.out.println(dDsum);
-        System.out.println(aDsum);
+        System.out.println("Descending Diagonal Sum: " + dDsum);
+        System.out.println("Ascending Diagonal Sum: " + aDsum);
         
+    	boolean isMcSq = isMagicSquare(rowsum,colsum,aDsum,dDsum);
+    	
+    	System.out.println("Is the Square a Magic Square: \n" + isMcSq);
     	
     }
     
